2e51ba7b51116a2f15d1189556fc5518ca13c9af
Kelly Duran <kaduran16@gmail.com>
2022-07-22 10:02:10 -0400

    commit tree 7ebab72d140d759e0b1ea4c1d9a7137fad21f3d6

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
+import React from 'react';
+
+
+function EyesOnMe (){
+    function handleFocus (){
+        console.log("Good!")
+    } 
+
+    function handleBlur (){
+        console.log("Hey! Eyes on me!")
+    }
+    
+    
+    return(
+        <div>
+            <button onFocus={handleFocus} onBlur={handleBlur}>Hey! Eyes on me!</button>
+        </div>
+    )
+
+}
+
+export default EyesOnMe
+
 // Code EyesOnMe Component Here
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
-// Code Keypad Component Here
+import React from 'react';
+function handleChange(){
+    console.log( "Entering password..." )
+   }
+
+function Keypad (){
+   
+    
+    
+    return(
+        <input onChange={handleChange} placeholder="enter password" type= "password"></input>
+    )
+
+}
+
+export default Keypad
